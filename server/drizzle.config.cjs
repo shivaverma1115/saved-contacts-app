e9062d4affdfef4780a99dbbe6f56cc285d56aa8
Shiva Verma <sverma@innovatrixinfotech.com>
2026-02-13 14:24:42 +0530

    backend issue fixed

---
+++ server/drizzle.config.cjs
@@ -0,0 +1,15 @@
+require("dotenv").config({ path: "./.env" });
+
+module.exports = {
+    schema: "./src/db/schema.ts",
+    out: "./drizzle",
+    dialect: "postgresql",
+    dbCredentials: {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        ssl: false
+    }
+};
